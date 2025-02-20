55d6d7a5c6fc9611b6101242878c3cf5969f521e
shivanand92 <129952258+shivanand92@users.noreply.github.com>
2025-02-20 13:25:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "fortios" {
   hostname = var.fortigate_hostname 
   vdom     = var.vdom        # Optional: Default is "root" VDOM
-   username = var.fortios_username       # Replace with your FortiGate admin username
+  username = var.fortios_username       # Replace with your FortiGate admin username
   password = var.fortios_password 
   insecure = true          
 }
