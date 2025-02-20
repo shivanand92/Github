4c7aa12306f25d20eb2c83dece2c0d6c2bd71bce
shivanand92 <129952258+shivanand92@users.noreply.github.com>
2025-02-20 13:24:31 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "fortios" {
+  hostname = var.fortigate_hostname 
+  vdom     = var.vdom        # Optional: Default is "root" VDOM
+   username = var.fortios_username       # Replace with your FortiGate admin username
+  password = var.fortios_password 
+  insecure = true          
+}
+
+
+resource "fortios_system_interface" "trname" {
+  ip           = "10.128.0.2/32"
+  name         = "VLAN1"         
+  type         = "vlan"           
+  vdom         = "root"
+  mode         = "static"         
+  allowaccess  = "ping"           
+  vlanid       = 1                 
+  description  = "VLAN1 Management Interface"
+}
+
