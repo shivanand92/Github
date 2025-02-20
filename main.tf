d792753aaad8f9f70f7ed58d1fe8ad1a32e2b57f
shivanand92 <129952258+shivanand92@users.noreply.github.com>
2025-02-20 15:39:04 +0530

    token

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 provider "fortios" {
   hostname = var.fortigate_hostname 
-  vdom     = var.vdom        # Optional: Default is "root" VDOM
-  username = var.fortios_username       # Replace with your FortiGate admin username
-  password = var.fortios_password 
+  token =var.api_token
+  vdom     = var.vdom       
   insecure = true          
 }
 resource "fortigate_system_global" "update"{
